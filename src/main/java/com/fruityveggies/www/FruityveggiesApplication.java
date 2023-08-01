2e1789645a9064dcc364b09ed9ab1f007819649d
ITWILL <ITWILL@DESKTOP-T6JOM6M>
2023-08-01 15:04:35 +0900

    지슈 커밋~!

---
+++ src/main/java/com/fruityveggies/www/FruityveggiesApplication.java
@@ -0,0 +1,13 @@
+package com.fruityveggies.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruityveggiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruityveggiesApplication.class, args);
+	}
+
+}
