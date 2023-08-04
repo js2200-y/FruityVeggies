d216b225d038c9e0fbf12c88db1d6084c8e6bf4b
kiminonawa <kiminonawa@1.1.1.31>
2023-08-04 15:26:05 +0900

    이노 커밋!

---
+++ src/main/java/com/fruityveggies/www/email/dto/EmailVerifyDto.java
@@ -0,0 +1,8 @@
+package com.fruityveggies.www.email.dto;
+
+import lombok.Data;
+
+@Data
+public class EmailVerifyDto {
+	private String email;
+}
---
+++ src/main/java/com/fruityveggies/www/web/EmailController.java
@@ -0,0 +1,31 @@
+package com.fruityveggies.www.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fruityveggies.www.service.EmailService;
+import com.fruityveggies.www.service.EmailServiceImpl;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class EmailController {
+	@Autowired
+	private EmailService emailService;
+	private EmailServiceImpl emailServiceImpl;
+	
+	
+	@PostMapping("/emailConfirm")
+	public String emailConfirm(@RequestParam String email) throws Exception {
+
+		log.info("email={}", email);
+		
+	  String confirm = emailService.sendSimpleMessage(email);
+	  log.info("emailServiceImpl.ePw={}",emailServiceImpl.ePw);
+
+	  return confirm;
+	}
+}
