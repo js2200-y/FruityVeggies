92a3954538023c539816a43abb821e062a6b7eb5
chklee1048 <chklee1048@gmail.com>
2023-08-11 15:18:16 +0900

    Merge branch 'master' of https://github.com/js2200-y/FruityVeggies.git

--- src/main/java/com/fruityveggies/www/service/ItemService.java
+++ src/main/java/com/fruityveggies/www/service/ItemService.java
@@ -2,17 +2,6 @@ package com.fruityveggies.www.service;
 
 import java.util.List;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.fruityveggies.www.repository.Item;
-import com.fruityveggies.www.repository.ItemOption;
-import com.fruityveggies.www.repository.ItemRepository;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +9,6 @@ import com.fruityveggies.www.dto.ItemItemOptionDto;
 import com.fruityveggies.www.repository.Item;
 import com.fruityveggies.www.repository.ItemRepository;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
