2411b5e1a621823a01898a5e9d522eafd818a194
zzong <zzong@DESKTOP-VGUS1CC>
2023-08-18 10:22:32 +0900

    정아 - rescueorder2

--- src/main/java/com/fruityveggies/www/web/RescueOrderController.java
+++ src/main/java/com/fruityveggies/www/web/RescueOrderController.java
@@ -1,10 +1,14 @@
 package com.fruityveggies.www.web;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.fruityveggies.www.dto.RescueOrderDto;
+import com.fruityveggies.www.repository.rescue.RescueOrder;
 import com.fruityveggies.www.service.RescueOrderService;
 
 import lombok.RequiredArgsConstructor;
@@ -25,9 +29,15 @@ public class RescueOrderController {
    }
    
    @GetMapping("/rescueorder/rescue_order2")
-   public String rescuorder2() {
+   public String rescuorder2(Model model) {
       
-	   log.info("rescuorder2 get");
+	   log.info("read()");
+	   
+	   List<RescueOrder> list = rescueOrderService.read();
+	   
+	   model.addAttribute("rescues", list);
+	   
+	   
       return "/rescueorder/rescue_order2";
    }
    
